0b097b532acd7ca064dbcd6f1dee6088c0ba114e
mar1n <30738216+mar1n@users.noreply.github.com>
2021-11-14 17:52:14 +0000

    databse connected

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import config from "config";
 import log from "./src/logger";
+import connect from "./src/db/connect";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -12,4 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
     log.info(`Server listing at http://${host}:${port}`);
+
+    connect();
 });
