3932905f00cdcaedb866f66ffc0f9bbaac5ef81e
mar1n <30738216+mar1n@users.noreply.github.com>
2021-11-19 19:13:01 +0000

    model user schema

---
+++ src/model/users.ts
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+// user schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    hash_password: {
+        type: String,
+        required: true,
+    },
+    salt: String,
+    role: {
+        type: String,
+        default: 'subscriber'
+    },
+    resetPasswordLink: {
+        data: String,
+        default: ''
+    }
+}, {timestamps: true});
+
+interface hash_pass {
+    _password: string,
+    _salt: string,
+    hashed_password: string,
+    makeSalt(): string,
+    encryptPassword(password: string): string
+}
+
+//virtual
+userSchema.virtual('password')
+.set(function(this: hash_pass, password: string) {
+    this._password = password;
+    this._salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+})
+.get(function(this: hash_pass) {
+    return this._password;
+})
+//methods
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hash_password;
+    },
+
+    encryptPassword: function(password) {
+        if(!password) return  '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch(err) {
+            return '';
+        }
+    },
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + ''
+    }
+}
+
+export default mongoose.model('User', userSchema);
